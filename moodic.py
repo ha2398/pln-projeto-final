8923b49d16b9d4ad6b77a1fc419c2b2aacdbe11b
Hugo Sousa <hsousa@gmx.com>
2017-09-12 18:03:25 -0300

    add argument parsing and lyric searching module

--- moodic.py
+++ moodic.py
@@ -5,7 +5,9 @@ Moodic
 @author: Hugo Sousa
 '''
 
+import argparse
 import lastplayed as lp
+import lyricwikia as lw
 import pylast
 import sys
 
@@ -14,12 +16,27 @@ API_SECRET = '93de978ac877b9c01079ce0cd32ae4ec'
 
 ###############################################################################
 
-def main():
-    if len(sys.argv) != 3:
-        return
 
-    username = sys.argv[1]
-    n_songs = int(sys.argv[2])
-    lp.get_recent_tracks(username, n_songs)
+def add_args():	
+	# Adds optional command line arguments to the program.
+	parser = argparse.ArgumentParser()
+	
+	parser.add_argument('USERNAME', metavar='username', type=str,
+		help='Name of the user to analyze')
+
+	parser.add_argument('-n', dest='NSONGS', default=20, type=int,
+		help='Number of last played songs to collect')
+
+	return parser.parse_args()
+
+
+def main():
+	args = add_args()
+	lp.get_recent_tracks(args.USERNAME, args.NSONGS)
+
+	lyrics = lw.get_lyrics('The Smiths', 'Stretch Out & Wait')
+	print(lyrics)
+
+	return
 
 main()
